791867879a5dea40cec550f9b99757af79a6a1d5
TanatornZ <bank250145.bb@gmail.com>
2024-10-30 10:40:37 +0700

    feat: config env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TextureModule } from './texture/texture.module';
+import { FirebaseModule } from './firebase/firebase.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [TextureModule],
+  imports: [TextureModule, FirebaseModule, ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
